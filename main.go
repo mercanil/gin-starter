5f2a994f60e91b39bd8d88ecb4637f9c8b185169
Anil Mercan <anil.mercan@ericsson.com>
2020-02-28 10:35:57 +0000

    router package added to orginize routing options

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
+	"./route"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-	router.GET("/", func(context *gin.Context) {
-		context.HTML(
-			http.StatusOK,
-			"index.html",
-			gin.H{
-				"title": "Anil Mercan Home Page",
-			},
-		)
-	})
-
+	route.InitializeRouter(router)
 	router.Run()
 }
---
+++ route/routes.go
@@ -0,0 +1,20 @@
+package route
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func InitializeRouter(router *gin.Engine)  {
+	router.GET("/" , showIndexPage)
+}
+
+func showIndexPage(context *gin.Context) {
+	context.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"title": "Anil Mercan Home Page",
+		},
+	)
+}
